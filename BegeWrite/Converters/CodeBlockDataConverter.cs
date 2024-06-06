6ecee7e04876bce794574fc330b8b60931c3d16b
Steve Temple <steve@gibedigital.com>
2024-06-06 10:09:05 +0100

    Hook up the backend and frontend code

---
+++ BegeWrite/Converters/CodeBlockDataConverter.cs
@@ -0,0 +1,51 @@
+﻿using System.Text.Json;
+
+namespace BegeWrite.Converters
+{
+    public class CodeBlockDataConverter : IBlockDataConverter
+    {
+        public bool CanConvert(string type)
+        {
+            return type.Equals("code");
+        }
+
+        public BegeWriteBlockBase Convert(ref Utf8JsonReader reader, string id, string type)
+        {
+            if (reader.TokenType != JsonTokenType.StartObject)
+            {
+                throw new JsonException();
+            }
+
+            var block = new BegeWriteBlock<CodeBlockData>(id, type);
+            block.Data = new CodeBlockData();
+            while (reader.Read())
+            {
+                if (reader.TokenType == JsonTokenType.EndObject)
+                {
+                    return block;
+                }
+
+                // Get the key.
+                if (reader.TokenType != JsonTokenType.PropertyName)
+                {
+                    throw new JsonException();
+                }
+
+                string? propertyName = reader.GetString() ?? "";
+                switch (propertyName.ToLower())
+                {
+                    case "code":
+                        reader.Read();
+                        block.Data.Code = reader.GetString() ?? "";
+                        break;
+                }
+            }
+            return block;
+        }
+    }
+
+    public class CodeBlockData
+    {
+        public string Code { get; set; }
+    }
+}
---
+++ BegeWrite/Converters/ListBlockDataConverter.cs
@@ -0,0 +1,66 @@
+﻿using System.Text.Json;
+
+namespace BegeWrite.Converters
+{
+    public class ListBlockDataConverter : IBlockDataConverter
+    {
+        public bool CanConvert(string type)
+        {
+            return type.Equals("list");
+        }
+
+        public BegeWriteBlockBase Convert(ref Utf8JsonReader reader, string id, string type)
+        {
+            if (reader.TokenType != JsonTokenType.StartObject)
+            {
+                throw new JsonException();
+            }
+
+            var block = new BegeWriteBlock<ListBlockData>(id, type);
+            block.Data = new ListBlockData();
+            while (reader.Read())
+            {
+                if (reader.TokenType == JsonTokenType.EndObject)
+                {
+                    return block;
+                }
+
+                // Get the key.
+                if (reader.TokenType != JsonTokenType.PropertyName)
+                {
+                    throw new JsonException();
+                }
+
+                string? propertyName = reader.GetString() ?? "";
+                switch (propertyName.ToLower())
+                {
+                    case "style":
+                        reader.Read();
+                        block.Data.Style = reader.GetString() ?? "";
+                        break;
+                    case "items":
+                        reader.Read();
+                        if (reader.TokenType == JsonTokenType.StartArray)
+                        {
+                            reader.Read();
+                            while (reader.TokenType != JsonTokenType.EndArray)
+                            {
+                                block.Data.Items.Add(reader.GetString() ?? "");
+                                reader.Read();
+                            }
+                        }
+
+
+                        break;
+                }
+            }
+            return block;
+        }
+    }
+
+    public class ListBlockData
+    {
+        public string Style { get; set; }
+        public List<string> Items { get; set; } = new List<string>();
+    }
+}
